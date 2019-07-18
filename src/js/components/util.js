6bb8554b1c818b58f512a0a60501e26f23ea2469
Okiki <femikiki@gmail.com>
2019-07-18 05:29:11 +0000

    Bug fixes

--- src/js/components/util.js
+++ src/js/components/util.js
@@ -7,12 +7,12 @@ const Util = {
     },
     pageSetup(url) {
         let routeName = Util.routeName(url);
-        
+
         // Check Active Link
         links.each(link => {
             let a = link.children[0];
             let hrefName = `${a.href}`.replace(`${a.origin}`, "");
-            
+
             // Clean class
             link.classList.remove('navbar-link-focus');
 
@@ -21,8 +21,8 @@ const Util = {
                 link.classList.add('navbar-link-focus');
             }
         });
-        
-        document.body.className = `page-${routeName}`;
+
+        document.body.className = `page-${routeName.includes("project") ? (routeName.includes("project-list") ? "project-list" : "project") : routeName}`;
     }
 };
 
