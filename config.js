c6ac1636841d568797d9df6f4d89407f0de15188
Okiki Ojo <femikiki@gmail.com>
2020-05-20 20:29:18 -0400

    Small update

--- config.js
+++ config.js
@@ -1,12 +1,18 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 if (process.env.NODE_ENV === "development") {
-    dotenv.config();
+  dotenv.config();
 }
 
 export const env = process.env;
-export const dev = 'dev' in env ? env.dev !== "true" : true;
-export const netlify = 'netlify' in env && env.netlify === "true";
-export const debug = 'debug' in env && env.debug === "true";
+export const dev = "dev" in env ? env.dev !== "true" : true;
+export const netlify = "netlify" in env && env.netlify === "true";
 export const websiteURL = "https://okikio.netlify.app";
-export { author, homepage, license, copyright, github, keywords } from "./package";
+export {
+  author,
+  homepage,
+  license,
+  copyright,
+  github,
+  keywords,
+} from "./package";
