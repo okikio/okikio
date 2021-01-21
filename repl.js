170eea39cc2446bb512e2c571282319add0b55ea
Okiki <femikiki@gmail.com>
2021-01-21 20:20:42 +0000

    chore: finalize Docker

--- repl.js
+++ repl.js
@@ -10,14 +10,14 @@
 //     console.log(pug);
 // })();
 
-const { webkit } = require("playwright");
+const { chromium } = require("playwright");
 
 (async () => {
-    const browser = await webkit.launch({ headless: false, slowMo: 50 });
+    const browser = await chromium.launch({ headless: false, slowMo: 50 });
     const page = await browser.newPage();
     await page.goto("http://whatsmyuseragent.org/");
     await page.screenshot({ path: `example.png` });
-    // await browser.close();
+    await browser.close();
 })();
 
 /**
