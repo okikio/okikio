07be3749ebb899aeac0597a53fd9ef56d3e7eb44
Okiki <femikiki@gmail.com>
2021-01-21 05:47:05 +0000

    ci: :green_heart: support Codespace Docker

--- repl.js
+++ repl.js
@@ -1,14 +1,21 @@
 // const promise1 = Promise.resolve({ default: 6 });
 // const promise2 = Promise.resolve({ default: 5 });
 // const promise3 = Promise.resolve({ default: 15 });
+// (async () => {
+//     const [{ default: pug }] = await Promise.all([
+//         Promise.resolve({ default: 6 }),
+//         Promise.resolve({ default: 5 }),
+//         Promise.resolve({ default: 15 }),
+//     ]);
+//     console.log(pug);
+// })();
+
+const { webkit } = require('playwright');
+
 (async () => {
-    const [{ default: pug }] = await Promise.all([
-        Promise.resolve({ default: 6 }),
-        Promise.resolve({ default: 5 }),
-        Promise.resolve({ default: 15 }),
-    ]);
-    console.log(pug);
+    const browser = await webkit.launch({ headless: false, slowMo: 50 });
+    const page = await browser.newPage();
+    await page.goto('http://whatsmyuseragent.org/');
+    await page.screenshot({ path: `example.png` });
+    await browser.close();
 })();
-
-
-default,es2015,es2016,es2017,es2018,Object.entries,fetch,IntersectionObserver,IntersectionObserverEntry,smoothscroll,matchMedia,localStorage,getComputedStyle,Element.prototype.scrollIntoView
