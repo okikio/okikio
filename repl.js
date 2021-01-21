50b17fb2e8976d34fde222e6e309b9ae798ec76a
Okiki <femikiki@gmail.com>
2021-01-21 07:16:15 +0000

    chore: wip test webkit on linux

--- repl.js
+++ repl.js
@@ -10,12 +10,34 @@
 //     console.log(pug);
 // })();
 
-const { chromium } = require('playwright');
+const { webkit } = require("playwright");
 
 (async () => {
-    const browser = await chromium.launch({ headless: false, slowMo: 50 });
+    const browser = await webkit.launch({ headless: false, slowMo: 50 });
     const page = await browser.newPage();
-    await page.goto('http://whatsmyuseragent.org/');
+    await page.goto("http://whatsmyuseragent.org/");
     await page.screenshot({ path: `example.png` });
-    await browser.close();
+    // await browser.close();
 })();
+
+/**
+ * const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+
+  // Open new page
+  const page = await context.newPage();
+
+  // Go to https://www.wikipedia.org/
+  await page.goto('https://www.wikipedia.org/');
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();
+
+*/
