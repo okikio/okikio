461fb764eaa9025c3ffc290971ac11bde84d4d94
Okiki Ojo <okikio.dev@gmail.com>
2022-01-31 16:18:55 +0000

    refactor: replace with astro (WIP)

--- src/ts/webanimation-polyfill.ts
+++
@@ -1,7 +0,0 @@
-if (!("KeyframeEffect" in window)) {
-    let script = document.createElement("script");
-    script.src = "./js/webanimation.min.js";
-    document.head.appendChild(script);
-}
-
-export { };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,61 +1,46 @@
 let weights = Array.from(Array(9), (_, i) => (i + 1) * 100);
 let fontWeight = {};
 weights.forEach((val) => {
-    fontWeight[val] = val;
+  fontWeight[val] = val;
 });
 
 module.exports = {
-    darkMode: "class",
-    mode: "jit",
-    purge: ["src/**/*.pug"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Manrope", "Century Gothic", "sans-serif"],
-                manrope: ["Manrope", "Century Gothic", "sans-serif"],
-                montserrat: ["Montserrat", "Century Gothic", "sans-serif"],
-            },
-            spacing: {
-                300: "300px",
-                400: "400px",
-                450: "450px",
-                500: "500px",
-            },
-            minHeight: (theme) => theme("spacing"),
-            minWidth: (theme) => theme("spacing"),
-            fontWeight,
-            colors: {
-                elevated: "#1C1C1E",
-                "elevated-2": "#262628",
-                label: "#ddd",
-                secondary: "#bbb",
-                tertiary: "#555",
-                quaternary: "#333",
-                "center-container-dark": "#121212",
+  darkMode: "media",
+  content: ["./src/**/*.{md,tsx,jsx,svelte,astro,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+        abril: ["Abril Fatface", "serif"],
+        manrope: ["Manrope", "sans-serif"],
 
-                primary: "#ffff00",
-                // secondary: "#FFC5C5",
-                // vibrant: "#FFE600",
-            },
-            screens: {
-                "3xl": "1633px",
-                "1.5xl": "1333px",
-                "lt-2xl": { max: "1535px" },
+        sans: ["Manrope", "sans-serif"],
+        serif: ["serif"],
+        title: ["Abril Fatface", "serif"],
+      },
+      fontWeight,
+      screens: {
+        "3xl": "1633px",
+        "1.5xl": "1333px",
+        "lt-2xl": { max: "1535px" },
 
-                "lt-xl": { max: "1279px" },
+        "lt-xl": { max: "1279px" },
 
-                "lt-lg": { max: "1023px" },
+        "lt-lg": { max: "1023px" },
 
-                "lt-md": { max: "767px" },
+        "lt-md": { max: "767px" },
 
-                "lt-sm": { max: "639px" },
+        "lt-sm": { max: "639px" },
 
-                "lt-xsm": { max: "339px" },
-            },
-            container: {
-                center: "true",
-            },
-        },
+        "lt-xsm": { max: "339px" },
+      },
+      container: {
+        center: "true",
+      },
+      colors: {
+        primary: "#ffff00"
+      }
     },
-    /* ... */
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
