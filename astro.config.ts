ed3f2b5683257b85e82ec0613bde753a105e6e7b
Okiki Ojo <okikio.dev@gmail.com>
2022-08-02 00:01:32 +0000

    chore: ...

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://okikio.dev",
   integrations: [sitemap()],
-  markdown: {
-    remarkPlugins: ["remark-gfm", "remark-code-titles", "remark-smartypants"],
-    rehypePlugins: [
-      "rehype-slug", 
-      ["rehype-autolink-headings", { behavior: "wrap" }]
-    ]
-  },
   vite: {
     ssr: {
       external: ["svgo"]
