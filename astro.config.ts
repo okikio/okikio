c54f465e0d9cb1d70c235b16c40e582f65160e95
Okiki Ojo <okikio.dev@gmail.com>
2023-05-28 01:46:43 +0000

    feat: add terms of service, privacy policy & rss feed

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
-import compress from "astro-compress";
+import compress from "astro-compress";            
+import mdx from "@astrojs/mdx";  
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://okikio.dev",
-  integrations: [sitemap(), compress({ css: false })],
+  integrations: [sitemap(), compress({ css: false }), mdx()],
   vite: {
     ssr: {
       external: ["svgo"]
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import type { APIContext } from 'astro';
+
+export async function get(context: APIContext) {  
+  return rss({
+    // `<title>` field in output xml
+    title: 'Okiki Ojo — Web Developer & Designer (okikio)',
+    // `<description>` field in output xml
+    description: 'A Web Developer and Designer based in Ontario, Canada, with an eye for great design (UI) and even better user experience (UX).',
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site.toString(),
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: [
+      ...await pagesGlobToRssItems(
+        import.meta.glob('./*.{md,mdx}'),
+      ),
+    ],
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
+}
